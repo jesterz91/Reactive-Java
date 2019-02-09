875c71dbd3fcb252208c1328bb58e0847ea8d256
jesterz91 <jesterz91@naver.com>
2019-02-09 18:45:13 +0900

    수학 및 기타 연산자

--- src/main/java/commons/CommonUtils.java
+++ src/main/java/commons/CommonUtils.java
@@ -1,10 +1,12 @@
 package commons;
 
+import java.util.Random;
+
 public class CommonUtils {
 
     public static long startTime;
 
-    public static void exampleStart(){
+    public static void exampleStart() {
         startTime = System.currentTimeMillis();
     }
 
@@ -20,6 +22,14 @@ public class CommonUtils {
     public static void sleep(int millis) {
         try {
             Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void doSomething() {
+        try {
+            Thread.sleep(new Random().nextInt(100));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
